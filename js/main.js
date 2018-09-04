7a84387172c3ff2066bc6b6eacb2a3334152892b
Jimmy Phan <jphan9@gmail.com>
2018-09-03 17:42:19 -0700

    fix: Fixed semantics for Aria and updated css styles.

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'Image of ' + restaurant.name + ' restaurant';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
